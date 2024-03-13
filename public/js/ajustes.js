18d5d551e81a3770456cd6106ee49a1fc0f8cde7
wild <wild.addsystem@gmail.com>
2024-03-13 12:01:29 -0400

    ACT
    ACT

--- public/js/ajustes.js
+++ public/js/ajustes.js
@@ -1,7 +1,63 @@
-const nombrePack = document.getElementById('nombrePack');
-// precio
-const precio = document.getElementById('precio');
-nombrePack.innerHTML = 'PACK DE ESTIMULACIÓN DE LENGUAJE (2-7 AÑOS)';
-precio.innerHTML = 'Precio: 10 USD';
+// Array de objetos con los datos
+const datos = [
+        {
+                nombreKit: 'PACK DE ESTIMULACIÓN DE LENGUAJE',
+                enlace: 'https://estimulacion-de-lenguaje.web.app/',
+                img: 'img/kit1.png'
+        },
+        {
+                nombreKit: 'KIT DE 1000 ACTIVIDADES',
+                enlace: 'https://mil-actividades.web.app/',
+                img: 'img/kit2.png'
+        },
+
+];
+
+// Obtener el contenedor donde se agregarán las tarjetas
+const contenedor = document.getElementById('contenedor');
+
+// Función para crear una tarjeta con los datos proporcionados
+function crearTarjeta(nombreKit, enlace, imgSrc) {
+        // Crear elementos HTML para la tarjeta
+        const divCard = document.createElement('div');
+        divCard.classList.add('card', 'card-kits', 'col-md-4');
+
+        const img = document.createElement('img');
+        img.src = imgSrc;
+        img.classList.add('card-img-top');
+        img.alt = '...';
+
+        const divCardBody = document.createElement('div');
+        divCardBody.classList.add('card-body');
+
+        const titulo = document.createElement('h5');
+        titulo.classList.add('card-title', 'text_card');
+        titulo.textContent = nombreKit;
+
+        const enlaceVerContenido = document.createElement('a');
+        enlaceVerContenido.href = enlace;
+        enlaceVerContenido.classList.add('btn');
+        enlaceVerContenido.textContent = 'Ver Contenido ';
+        // agREGAR ICONO
+        const icono = document.createElement('i');
+        icono.classList.add('fas', 'fa-eye');
+        enlaceVerContenido.appendChild(icono);
+
+
+        // Agregar elementos a la tarjeta
+        divCardBody.appendChild(titulo);
+        divCardBody.appendChild(enlaceVerContenido);
+
+        divCard.appendChild(img);
+        divCard.appendChild(divCardBody);
+
+        // Agregar la tarjeta al contenedor
+        contenedor.appendChild(divCard);
+}
+
+// Recorrer el array de datos y crear una tarjeta para cada objeto
+datos.forEach(dato => {
+        crearTarjeta(dato.nombreKit, dato.enlace, dato.img);
+});
 
 
